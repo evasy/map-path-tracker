6b12c05b103f3ba4f88219fda3c8982cbabf21df
evasy <yitongshan2016@yahoo.com>
2023-07-10 22:58:01 -0700

    start/pause and update location every 3s (todo: debug file path)

--- app/src/main/java/com/example/path_tracker/MapsActivity.java
+++ app/src/main/java/com/example/path_tracker/MapsActivity.java
@@ -13,7 +13,10 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -27,7 +30,12 @@ import com.example.path_tracker.databinding.ActivityMapsBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.security.spec.ECField;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -36,7 +44,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     private FusedLocationProviderClient mFusedLocationClient;
     // List<Address> listGeoCoder;
-    private static final int LOCATION_PERMISSION_CODE = 1027;
+    private static final int LOCATION_PERMISSION_CODE = 1028;
+    private static final String TAG = "MapsActivity";
+
+    // record the coordinates, and a state of 'obstacled' or not
+    private ArrayList<String> coordinates = new ArrayList<>();
+
+    private double longitude;
+    private double latitude;
+
+    // for executing periodically (every 3 seconds)
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+    private Button btnStartPause;
+
+    private boolean isUpdatingLocation = false; // state of whether the location is updating
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +70,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        btnStartPause = findViewById(R.id.btnStartPause);
+        btnStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isUpdatingLocation) {
+                    startLocationUpdates();
+                    btnStartPause.setText("Pause");
+                } else {
+                    stopLocationUpdates();
+                    btnStartPause.setText("Start");
+                }
+                isUpdatingLocation = !isUpdatingLocation;
+            }
+        });
+
         if (isLocationPermissionGranted()) {
             // Obtain the SupportMapFragment and get notified when the map is ready to be used.
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -52,19 +92,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mapFragment.getMapAsync(this);
 
             getLocation();
-
-//            try {
-//                listGeoCoder = new Geocoder(this).getFromLocationName("University of Washington",1);
-//            }
-//            catch (Exception e) {
-//                e.printStackTrace();
-//            }
-
-//            double longitude = listGeoCoder.get(0).getLongitude();
-//            double latitude = listGeoCoder.get(0).getLatitude();
-//
-//            Log.i("GOOGLE_MAP_TAG", "Address has Longitude " + ":::" +
-//                    String.valueOf(longitude) + " Latitude " + String.valueOf(latitude));
         }
         else {
             requestLocationPermission();
@@ -73,57 +100,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private void getLocation (){
-        Log.d(TAG, "get the device's current location");
+    private void getLocation() {
+        Log.d(TAG, "Get the device's current location");
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         try {
             if (isLocationPermissionGranted()) {
                 Task location = mFusedLocationClient.getLastLocation();
-                location.addOnCompleteListener(new OnCompleteListener(){
+                location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
                     public void onComplete(Task task) {
                         if (task.isSuccessful()) {
                             Location currentLocation = (Location) task.getResult();
-                            double longitude = currentLocation.getLongitude();
-                            double latitude = currentLocation.getLatitude();
-                            Log.i("GOOGLE_MAP_TAG", "Current location coordinate (latitude, longitude): "+
-                                String.valueOf(latitude) + ", " + String.valueOf(longitude));
-                        }
-                        else {
-                            Log.d(TAG, "location is null");
+                            if (currentLocation != null) {
+                                longitude = currentLocation.getLongitude();
+                                latitude = currentLocation.getLatitude();
+                                Log.i("GOOGLE_MAP_TAG", "Current location coordinate (latitude, longitude): " +
+                                        String.valueOf(latitude) + ", " + String.valueOf(longitude));
+                            } else {
+                                Log.d(TAG, "Location is null");
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting location: " + task.getException().getMessage());
                         }
                     }
 
                 });
             }
 
-        }
-        catch (SecurityException e){
+        } catch (SecurityException e) {
             Log.e(TAG, e.getMessage());
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
+            getLocation(); // Retrieve current location
+        } else {
+            requestLocationPermission();
         }
 
     }
@@ -141,4 +158,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void requestLocationPermission() {
         ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
     }
+
+    private void startLocationUpdates() {
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                getLocation();
+                String newLocation;
+                newLocation = String.valueOf(latitude) + "," + String.valueOf(longitude) + "," ; // time stamp
+                coordinates.add(newLocation);
+                mHandler.postDelayed(this, 3000); // Update every 3 seconds
+            }
+        };
+        mHandler.postDelayed(mRunnable, 0); // Start immediately
+    }
+
+    private void stopLocationUpdates() {
+        if (mHandler != null && mRunnable != null) {
+            mHandler.removeCallbacks(mRunnable);
+            mHandler = null;
+            mRunnable = null;
+        }
+        System.out.println(coordinates);
+        writeToFile();
+        coordinates.clear();
+    }
+
+        private void writeToFile() {
+        String filename = "file.txt"; // TODO: make it show in date and time
+        String filepath = "/Users/yitongshan/AndroidStudioProjects/pathtracker/app/RecordedPaths/" + filename;
+        try {
+            FileWriter fileWriter = new FileWriter(filepath);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            for (String location : coordinates) {
+                bufferedWriter.write(location);
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.close();
+            System.out.println("Data written to the file successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
